29955cfc351b07660d958eb15b54bb49f36bda13
RachidHallouti <hallotir531@gmail.com>
2026-06-26 00:55:14 +0100

    cocktails section

--- src/components/Cocktails.tsx
+++ src/components/Cocktails.tsx
@@ -1,5 +1,73 @@
-import React from "react"
+import { useGSAP } from "@gsap/react"
+import { cocktailLists, mockTailLists } from "../../constants"
+import gsap from "gsap"
 
 export default function Cocktails() {
-  return <div>Cocktails</div>
+  useGSAP(() => {
+    const paralaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#cocktails",
+        start: "top 30%",
+        end: "bottom 80%",
+        scrub: true,
+      },
+    })
+
+    paralaxTimeline.from("#c-left-leaf", {
+      x: -100,
+      y: 100,
+    })
+    paralaxTimeline.from("#c-right-leaf", {
+      x: 100,
+      y: 100,
+    })
+  }, [])
+  return (
+    <section id="cocktails" className="noisy">
+      <img
+        src="/images/cocktail-left-leaf.png"
+        alt="left-leaf"
+        id="c-left-leaf"
+      />
+      <img
+        src="/images/cocktail-right-leaf.png"
+        alt="right-leaf"
+        id="c-right-leaf"
+      />
+      <div className="list">
+        <div className="popular">
+          <h2>Most popular cocktails</h2>
+          <ul>
+            {cocktailLists.map((c) => (
+              <li key={c.name}>
+                <div className="md:me-28">
+                  <h3>{c.name}</h3>
+                  <p>
+                    {c.country} | {c.detail}
+                  </p>
+                </div>
+                <span>- {c.price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="loved">
+          <h2>Most loved mocktails</h2>
+          <ul>
+            {mockTailLists.map((c) => (
+              <li key={c.name}>
+                <div className="me-28">
+                  <h3>{c.name}</h3>
+                  <p>
+                    {c.country} | {c.detail}
+                  </p>
+                </div>
+                <span>- {c.price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
 }
